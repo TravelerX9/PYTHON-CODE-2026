2aea315a355b565020abb9a618a42952bf1f1c1f
Romanza <fontanafreedom9@gmail.com>
2026-01-04 12:42:41 +0800

    Update main_alias.py

--- PYTHON CODE 2026/Episode 55 - Membuat Module/main_alias.py
+++ PYTHON CODE 2026/Episode 55 - Membuat Module/main_alias.py
@@ -11,7 +11,7 @@ hasil_tambah = add(1,2,3,4,5)
 print(f"hasil tambah = {hasil_tambah}")
 
 hasil_kali = math.kali(1,2,3,4,5)
-print(f"hasil tambah = {hasil_kali}")
+print(f"hasil kali = {hasil_kali}")
 
 pangkat_3 = terserah_kalian_mau_apa(3)
 print(f"hasil pangkat3 = {pangkat_3(3)}")
