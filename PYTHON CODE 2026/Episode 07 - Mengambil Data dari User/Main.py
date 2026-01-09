1a91fa05549a405cb2817f011b10429aeb20829d
Romanza <fontanafreedom9@gmail.com>
2026-01-09 11:39:48 +0800

    Update Main.py

--- PYTHON CODE 2026/Episode 07 - Mengambil Data dari User/Main.py
+++ PYTHON CODE 2026/Episode 07 - Mengambil Data dari User/Main.py
@@ -5,7 +5,7 @@ data = input("Masukan data: ")
 
 print("data = ",data,",type =",type(data))
 
-# jika kita ingin mengambil int, maka
+# jika kita ingin mengambil int, maka dilanjutkan
 angka = float(input("masukan angka: "))
 angka = int(input("masukan angka: "))
 
